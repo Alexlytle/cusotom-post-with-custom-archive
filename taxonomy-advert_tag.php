1be70e04667faa4879266ebdb23e137373ca5903
Alexlytle <44213950+Alexlytle@users.noreply.github.com>
2021-10-15 16:42:42 -0700

    Update taxonomy-advert_tag.php

--- taxonomy-advert_tag.php
+++ taxonomy-advert_tag.php
@@ -7,17 +7,57 @@ get_header();
 
 
 <?php 
-
+$taxonomy     = 'advert_tag';
+$orderby      = 'name'; 
+$show_count   = false;
+$pad_counts   = false;
+$hierarchical = true;
+$title        = '';
+$args = array(
+    'taxonomy'     => $taxonomy,
+    'orderby'      => $orderby,
+    'show_count'   => $show_count,
+    'pad_counts'   => $pad_counts,
+    'hierarchical' => $hierarchical,
+    'title_li'     => $title
+  );
+  
+  
+    
 
   
-$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
-
 
     // die();
 
+    ?>
+    <h2 style="margin: 0;">Bread Crumb</h2>
+    <?php
+    $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
     echo get_term_parents_list( $term->term_id,  $term->taxonomy);
 
 
+    $terms = get_terms([
+        'taxonomy' => $term->taxonomy,
+        'hide_empty' => false,
+        // 'order'=>'asc'
+    ]);
+    
+    
+    ?>
+    <h2 style="margin: 0;">Menu</h2>
+    <?php
+   wp_list_categories( $args );
+    ?>
+
+<div class="">
+<?php
+
+?>
+
+</div>
+
+    
+<?php
 $the_query = new WP_Query( array(
   'post_type' => 'Adverts',
   'tax_query' => array(
@@ -25,10 +65,11 @@ $the_query = new WP_Query( array(
           'taxonomy' => $term->taxonomy,
           'field' => 'slug',
           'terms' => $term->name,
-          'include_children' => true, 
+          'include_children' => false, 
       )
   ),
-) );
+) 
+);
 
 while ( $the_query->have_posts() ) :
     $the_query->the_post();
